2ad252caf68e8c7b44e3c177307d48576f948c05
Yash <devsytem123@gmail.com>
2025-12-24 16:59:20 +0530

    email fixed

--- config/email.js
+++ config/email.js
@@ -1,49 +1,22 @@
-// import nodemailer from 'nodemailer'
-// const sendEmail=async(email,subject,message)=>{
-//     const transporter=nodemailer.createTransport({
-//         service: "gmail",
+import nodemailer from 'nodemailer'
+const sendEmail=async(email,subject,message)=>{
+    const transporter=nodemailer.createTransport({
+        service: "gmail",
         
-//         auth:{
-//             user:process.env.EMAIL_USER,
-//             pass:process.env.EMAIL_PASS
-//         }
-//     })
+        auth:{
+            user:process.env.EMAIL_USER,
+            pass:process.env.EMAIL_PASS
+        }
+    })
 
-//     await transporter.sendMail({
-//         from:process.env.EMAIL_USER,
-//         to:email,
-//         subject,
-//         text:message
-//     })
-// }
-
-
-
-// export default sendEmail
+    await transporter.sendMail({
+        from:process.env.EMAIL_USER,
+        to:email,
+        subject,
+        text:message
+    })
+}
 
 
 
-import { Resend } from 'resend';
-
-
-const resend = new Resend(process.env.RESEND_KEY);
-
-const sendEmail = async (email, subject, message) => {
-  try {
-    await resend.emails.send({
-      from: 'devsytem123@gmail.com',
-      to: email,
-      subject,
-      text: message,
-    });
-
-    console.log('✅ Email sent via Resend');
-  } catch (error) {
-    console.error('❌ Resend email failed:', error);
-    throw error;
-  }
-};
-
-export default sendEmail;
-
-
+export default sendEmail
